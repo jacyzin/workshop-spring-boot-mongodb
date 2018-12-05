6be2b5397eb4a18f380d5594496f50217bc0296e
kodemann <henryjacyzin@gmail.com>
2018-12-04 22:56:52 -0200

    first commit

---
+++ src/main/java/com/jacyzin/workshopspringbootmongodb/WorkshopspringbootmongodbApplication.java
@@ -0,0 +1,12 @@
+package com.jacyzin.workshopspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringbootmongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopspringbootmongodbApplication.class, args);
+    }
+}
