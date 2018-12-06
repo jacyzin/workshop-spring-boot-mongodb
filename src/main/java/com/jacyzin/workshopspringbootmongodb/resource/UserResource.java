83a260fc4d3c0ba20879c2c78c8bc642112ef687
kodemann <henryjacyzin@gmail.com>
2018-12-06 21:58:20 -0200

    added support for insert to database.

--- src/main/java/com/jacyzin/workshopspringbootmongodb/resource/UserResource.java
+++ src/main/java/com/jacyzin/workshopspringbootmongodb/resource/UserResource.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,6 +29,22 @@ public class UserResource {
         List<UserDTO> usersDTO = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
         return ResponseEntity.ok().body(usersDTO);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        Optional<User> user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user.get()));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+
 
 
 
--- src/main/java/com/jacyzin/workshopspringbootmongodb/service/UserService.java
+++ src/main/java/com/jacyzin/workshopspringbootmongodb/service/UserService.java
@@ -3,11 +3,12 @@ package com.jacyzin.workshopspringbootmongodb.service;
 import com.jacyzin.workshopspringbootmongodb.domain.User;
 import com.jacyzin.workshopspringbootmongodb.dto.UserDTO;
 import com.jacyzin.workshopspringbootmongodb.repository.UserRepository;
-import com.jacyzin.workshopspringbootmongodb.resource.UserResource;
+import com.jacyzin.workshopspringbootmongodb.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -19,5 +20,19 @@ public class UserService {
         return userRepository.findAll();
     };
 
+    public Optional<User> findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent())
+            throw new ObjectNotFoundException("Objeto não encontrado");
 
+        return user;
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
 }
